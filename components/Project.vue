ff2b18bcce1de3fb807fc340fd6690b4771fb6d2
Felix Montanari <captainboggle108@gmail.com>
2024-04-02 14:57:05 +1100

    Implement improvement from feedback about project clickability

--- components/Project.vue
+++ components/Project.vue
@@ -1,36 +1,29 @@
-<script setup lang="ts">
-import type { Project } from '~/github';
-
-defineProps<{
-  project: Project;
-}>();
-</script>
-
 <template>
-  <div
-    class="flex flex-col gap-1 rounded bg-white-secondary p-4 dark:bg-white-primary"
+  <div 
+    class="flex flex-col gap-1 rounded bg-white-secondary p-4 dark:bg-white-primary cursor-pointer" 
+    @click="openProject"
   >
-    <a
-      class="text-dark-primary text-xl font-semibold dark:text-black-primary"
-      :href="project?.url ?? project?.homepageUrl ?? '#'"
-      rel="noreferrer noopener"
-      target="_blank"
-      >{{ project.name }}</a
-    >
+    <span class="text-dark-primary text-xl font-semibold dark:text-black-primary">{{ project.name }}</span>
 
-    <p
-      class="text-dark-secondary dark:text-black-primary"
-      :class="{ 'mb-5': project?.languages?.length }"
-    >
+    <p class="text-dark-secondary dark:text-black-primary" :class="{ 'mb-5': project?.languages?.length }">
       {{ project.description }}
     </p>
 
     <div v-if="project?.languages?.length" class="mt-auto flex gap-2">
-      <ProjectLanguage
-        v-for="(language, languageIndex) of project?.languages ?? []"
-        :key="languageIndex"
-        :language="language"
-      />
+      <ProjectLanguage v-for="(language, languageIndex) of project?.languages ?? []" :key="languageIndex"
+        :language="language" />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import type { Project } from '~/github';
+
+const props = defineProps<{
+  project: Project;
+}>();
+
+const openProject = () => {
+  window.open(props.project?.url ?? props.project?.homepageUrl ?? '#', '_blank');
+};
+</script>
